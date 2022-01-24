0235a41dbbc37ec7925249c5cfa510e851067fdf
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2022-01-24 19:59:40 -0300

    Desafio01DatabaseQueries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,32 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      relations: ["games"],
+      where: { id: user_id }
+    })
+
+    if (!user) throw new Error("Invalid Id");
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return  await this.repository.query(`
+    select *
+    from users
+    order by users.first_name
+  `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const user = await this.repository.query(`
+    SELECT * FROM users
+    WHERE LOWER(users.first_name) = LOWER($1)
+    AND LOWER(users.last_name) = LOWER($2)
+  `, [first_name, last_name]); 
+  return user; 
   }
 }
